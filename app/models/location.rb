3b6116b144c2ea46631e0699d30175dab312ef1a
guillermo varela <guillovarela16@gmail.com>
2019-02-10 11:19:47 -0300

    geocoder gem installed

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,6 @@
 class Location < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true, uniqueness: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
