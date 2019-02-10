ff60ce6351304651cb540fe8138a0fae44c469a2
guillermo varela <guillovarela16@gmail.com>
2019-02-09 21:07:13 -0300

    controller and seed finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'clearing database'
+Location.destroy_all
+puts 'creating locations'
+
+Location.create!(name: "Nazare", address: "Estr. do Farol, Nazaré, Portugal", photo: "https://i0.wp.com/blog.nuestroclima.com/wp-content/uploads/2015/10/Olas-gigantes-en-Portugal-1.png?w=1000")
+Location.create!(name: "Bayron Bay", address: "1 Brooke Dr, Byron Bay NSW 2481, Australia", photo: "https://www.rtwbackpackers.com/wp-content/uploads/2016/08/ultimate-backpackers-guide-to-byron-bay-australia-surfing-gap-year-east-coast-1200x520.jpg")
+
+puts 'finished'
