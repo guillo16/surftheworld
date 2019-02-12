0bfec742c81d8ecf7237fc97dc5f0246f9df6d5d
guillermo varela <guillovarela16@gmail.com>
2019-02-12 10:36:10 -0300

    create video controller and routes

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,12 @@
+class VideosController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @videos = Video.all
+  end
+
+  def show
+    @video = Video.find(params[:id])
+  end
+
+end
