136f6462fa37c277b3705b419bca353b8ece0249
guillermo varela <guillovarela16@gmail.com>
2019-08-15 14:59:05 -0300

    finish fixing mayor errors

--- db/schema.rb
+++ db/schema.rb
@@ -49,18 +49,6 @@ ActiveRecord::Schema.define(version: 2019_04_08_151540) do
     t.float "longitude"
   end
 
-  create_table "titles", force: :cascade do |t|
-    t.string "subtitle"
-    t.text "main_article"
-    t.text "second_article"
-    t.string "writer"
-    t.string "photo_url"
-    t.string "photo"
-    t.string "second_photo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
